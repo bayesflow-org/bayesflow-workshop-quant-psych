f8ca826902f6a5487ecf0ed145b4371cdc05b89e
Kucharssim <kucharssim@gmail.com>
2025-02-11 18:24:45 +0100

    improve normalizing flow slides and exercise

--- figures/normalizing-flow/flow-composition.R
+++ figures/normalizing-flow/flow-composition.R
@@ -13,26 +13,21 @@ par(oma=c(0,0,0,0), mar=c(0,0,0,0), mfrow=c(1, 1))
 
 curve(
   dmix(x=x,c(5, 4, 2, 3), c(-2.5, -1, 1, 3), c(0.9, 0.3, 0.7, 0.5)),
-  from=-5, to=5, n=1000, lwd=3,
-  axes=FALSE, xlab="", ylab="")
-
-curve(
-  dmix(x=x,c(5, 4, 2, 3), c(-2.3, -1, 1, 3), c(0.9, 0.3, 1.1, 0.5)),
-  from=-5, to=5, n=1000, lwd=3,
+  from=-5, to=5, n=1000, lwd=10,
   axes=FALSE, xlab="", ylab="")
 
 curve(
   dmix(x=x,c(5, 4, 3), c(-1.8, -1, 2.5), c(1.1, 0.4, 0.7)),
-  from=-5, to=5, n=1000, lwd=3,
+  from=-5, to=5, n=1000, lwd=10,
   axes=FALSE, xlab="", ylab="")
 
 curve(
   dmix(x=x,c(5, 3), c(-1, 3), c(1.5, 1)),
-  from=-5, to=5, n=1000, lwd=3,
+  from=-5, to=5, n=1000, lwd=10,
   axes=FALSE, xlab="", ylab="")
 
 curve(
   dmix(x=x,1, 0, 1),
-  from=-5, to=5, n=1000, lwd=3,
+  from=-5, to=5, n=1000, lwd=10,
   axes=FALSE, xlab="", ylab="")
 
